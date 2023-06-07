6eb37a8b59d0ea163a7af54ece0541df04221dd5
sanchezj <53372645+Vredeza@users.noreply.github.com>
2023-06-07 12:35:19 +0000

    Updating scripts

--- scripts/create-bucket.sh
+++ scripts/create-bucket.sh
@@ -1,1 +1,16 @@
-../templates/scripts
+#!/bin/sh
+
+source ../.env
+
+# (!!) Change default password
+MINIO_NET="dsw-inrae-deployment_default"
+MINIO_BUCKET="engine-wizard"
+
+sudo docker run --rm --net $MINIO_NET \
+  -e MINIO_BUCKET=$MINIO_BUCKET \
+  -e MINIO_USER="$MINIO_ROOT_USER" \
+  -e MINIO_PASS="$MINIO_ROOT_PASSWORD" \
+  --entrypoint sh minio/mc -c "\
+  mc config host add dswminio http://minio:9000 \$MINIO_USER \$MINIO_PASS && \
+  mc mb dswminio/\$MINIO_BUCKET
+"
